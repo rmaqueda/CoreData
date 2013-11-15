477b1a7fa68cca0a585e1ef0a76ad83f4988b352
Ricardo Maqueda <rinconestolmo@gmail.com>
2013-11-15 04:45:41 +0100

    Test Data from multiples views

--- CoreDataStuff/CoreDataStuff/RMMFirstViewViewController.m
+++ CoreDataStuff/CoreDataStuff/RMMFirstViewViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "RMMFirstViewViewController.h"
+#import "RMMAppDelegate.h"
+#import "RMMSecondViewController.h"
 
 @interface RMMFirstViewViewController ()
 
@@ -14,6 +16,7 @@
 
 @implementation RMMFirstViewViewController
 //@synthesize nameText;
+@synthesize managedObjectContext;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -55,15 +58,57 @@
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
     
+    NSManagedObject *name = [NSEntityDescription insertNewObjectForEntityForName:@"Event" inManagedObjectContext:self.managedObjectContext];
+    [name setValue:@"ppp" forKey:name];
 
-    
-    
-    
-//    if ([[segue identifier] isEqualToString:@"showDetail"]) {
-//        NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
-//        NSManagedObject *object = [[self fetchedResultsController] objectAtIndexPath:indexPath];
-//        [[segue destinationViewController] setDetailItem:object];
-//    }
 }
+
+
+- (NSFetchedResultsController *)fetchedResultsController
+{
+    if (_fetchedResultsController != nil) {
+        return _fetchedResultsController;
+    }
+    
+    NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] init];
+    // Edit the entity name as appropriate.
+    NSEntityDescription *entity = [NSEntityDescription entityForName:@"Event" inManagedObjectContext:self.managedObjectContext];
+    [fetchRequest setEntity:entity];
+    
+    // Set the batch size to a suitable number.
+    //[fetchRequest setFetchBatchSize:20];
+    
+    // Edit the sort key as appropriate.
+//    NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"timeStamp" ascending:NO];
+//    NSArray *sortDescriptors = @[sortDescriptor];
+//    
+//    [fetchRequest setSortDescriptors:sortDescriptors];
+    
+    // Edit the section name key path and cache name if appropriate.
+    // nil for section name key path means "no sections".
+//    NSFetchedResultsController *aFetchedResultsController = [[NSFetchedResultsController alloc] initWithFetchRequest:fetchRequest managedObjectContext:self.managedObjectContext sectionNameKeyPath:nil cacheName:@"Master"];
+//    //aFetchedResultsController.delegate = self;
+//    self.fetchedResultsController = aFetchedResultsController;
+    
+//	NSError *error = nil;
+//	if (![self.fetchedResultsController performFetch:&error]) {
+//        // Replace this implementation with code to handle the error appropriately.
+//        // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+//	    NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+//	    abort();
+//	}
+    
+    return _fetchedResultsController;
+}
+
+- (NSManagedObjectContext *)managedObjectContext {
+    NSManagedObjectContext *context = nil;
+    id delegate = [[UIApplication sharedApplication] delegate];
+    if ([delegate performSelector:@selector(managedObjectContext)]) {
+        context = [delegate managedObjectContext];
+    }
+    return context;
+}
+
 
 @end
--- CoreDataStuff/CoreDataStuff/RMMSecondViewController.m
+++ CoreDataStuff/CoreDataStuff/RMMSecondViewController.m
@@ -29,11 +29,7 @@
 	// Do any additional setup after loading the view.
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+
 
 - (IBAction)saveButtom:(id)sender {
 }
